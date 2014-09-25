9aa5157fb8e5116f229056ee4d3cc3d75e0386db
Kengo NAKAHARA <k-nakahara@iij.ad.jp>
2014-09-25 05:52:22 +0000

    [if_vmx] support msi

--- sys/arch/x86/pci/if_vmxreg.h
+++ sys/arch/x86/pci/if_vmxreg.h
@@ -233,6 +233,9 @@ struct vmxnet3_rxcompdesc {
 #define VMXNET3_MAX_MTU 9000
 #define VMXNET3_MIN_MTU 60
 
+#define VMXNET3_IT_LEGACY 0x01
+#define VMXNET3_IT_MSI    0x02
+
 struct vmxnet3_driver_shared {
 	uint32_t magic;
 	uint32_t pad1;
